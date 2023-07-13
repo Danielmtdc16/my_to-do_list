2744d53dcebd17b6096a956b34b0bc85cceb4877
Daniel <danielmtdc16@gmail.com>
2023-07-13 13:17:16 -0300

    função para concluir uma tarefa.

--- assets/script/script.js
+++ assets/script/script.js
@@ -28,7 +28,7 @@ function showATask() {
     let newLi = "";
     arrayTask.forEach((element, index) => {
         newLi = newLi + `
-        <li class="task"><i class="uil uil-check-circle check icon-task"></i>${element}<i class="uil uil-trash-alt delete icon-task" onclick="deleteTask(${index})"></i></li>
+        <li class="task"><i class="uil uil-check-circle check icon-task" onclick="conclude(${index})"></i>${element}<i class="uil uil-trash-alt delete icon-task" onclick="deleteTask(${index})"></i></li>
         `;
     });
     list_task.innerHTML = newLi;
@@ -48,5 +48,13 @@ function deleteTask(index) {
         `;
     }
 }
+
+function conclude(index) {
+    const task = arrayTask[index];
+    arrayTask.splice(index, 1);
+    arrayTask.push(task);
+    showATask();
+}
+
 
 button.addEventListener('click', addTask);
